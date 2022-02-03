c183bdc52195a16f58087d7621712343f38a0b25
AdrianGonzalezSO <adrian.gonzalez@softwareone.com>
2022-02-03 11:21:33 +0100

    adding logo

--- js/script.js
+++ js/script.js
@@ -5,8 +5,9 @@ var speed = 60;
 typewriter = () => {
     document.querySelector("#message").innerHTML = text[0].substring(0, textPosition) + '<span>\u25AE</span>';
     console.log(text);
-    if(textPosition++ != text[0].length)
+    if(textPosition++ != text[0].length){
       setTimeout(typewriter, speed);
-  }
-  
-  window.addEventListener("load", typewriter)
+    } 
+}
+
+window.addEventListener("load", typewriter)
