9cf4cb3c14d61eb212202a21eea0c12f99541f2e
AdrianGonzalezSO <adrian.gonzalez@softwareone.com>
2022-02-03 09:20:21 +0100

    Change speed text

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var text = ["Hola, soy Adrián González, un desarrollador de aplicaciones web nacido en Ponferrada"];
 var textPosition = 0; 
-var speed = 150;
+var speed = 60;
 
 typewriter = () => {
     document.querySelector("#message").innerHTML = text[0].substring(0, textPosition) + '<span>\u25AE</span>';
