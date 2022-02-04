e8489105b2832d5a8c9405d90748dcd2e1b7c1be
AdrianGonzalezSO <adrian.gonzalez@softwareone.com>
2022-02-04 11:22:21 +0100

    minor change

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var text = ["Hola, soy Adrián González, un principio de desarrollador de aplicaciones web nacido en Ponferrada. Actualmente me encuentro realizando prácticas en SoftwareOne."];
 var textPosition = 0;
-var speed = 60;
+var speed = 65;
 
 typewriter = () => {
     document.querySelector("#message").innerHTML = text[0].substring(0, textPosition) + '<span>\u25AE</span>';
